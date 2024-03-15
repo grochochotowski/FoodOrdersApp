8286640879ed91fc8c699561c2e7997ae3c73899
grochochotowski <g.m.rochowski@gmail.com>
2024-03-15 15:38:44 +0100

    create user model

---
+++ FoodOrdersApi/Models/UserDto.cs
@@ -0,0 +1,13 @@
+﻿namespace FoodOrdersApi.Models
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string? SecondName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int OrganizationId { get; set; }
+        public string? Note { get; set; }
+    }
+}
