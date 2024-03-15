350ff81fec615fb34b124d85195f5d7098e5dc3e
grochochotowski <g.m.rochowski@gmail.com>
2024-03-15 15:59:27 +0100

    create org controller

---
+++ FoodOrdersApi/Services/OrgService.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using FoodOrdersApi.Entities;
+using FoodOrdersApi.Models;
+
+namespace FoodOrdersApi.Services
+{
+    public interface IOrgService
+    {
+        int Create(CreateOrgDto dto);
+    }
+
+    public class OrgService : IOrgService
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public OrgService(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+
+        public int Create(CreateOrgDto dto)
+        {
+            var org = _mapper.Map<User>(dto);
+            _context.Organizations.Add(org);
+            _context.SaveChanges();
+
+            return org.Id;
+        }
+    }
+}
