9e322b2eb02a3939df8467c10d24f24b561a00d4
grochochotowski <g.m.rochowski@gmail.com>
2024-03-15 14:08:42 +0100

    update user with org

--- FoodOrdersApi/Entities/User.cs
+++ FoodOrdersApi/Entities/User.cs
@@ -6,13 +6,24 @@ namespace FoodOrdersApi.Entities
     {
         [Key]
         public int Id { get; set; }
+
         [Required(ErrorMessage ="First name is requierd")]
         public string FirstName { get; set; }
+
         public string? SecondName { get; set; }
+
         [Required(ErrorMessage = "Last name is requierd")]
         public string LastName { get; set; }
+
         [Required(ErrorMessage = "Email is requierd")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Organization ID is requierd")]
+        public int OrganizationId { get; set; }
+
         public string? Note { get; set; }
+
+
+        public virtual Organization Organization { get; set; }
     }
 }
