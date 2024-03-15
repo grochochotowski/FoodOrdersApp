23f9d1c63e54026210ff6eef64a478d02b62c2b4
grochochotowski <g.m.rochowski@gmail.com>
2024-03-15 13:37:03 +0100

    create db context

--- FoodOrdersApi/Entities/AppContext.cs
+++ FoodOrdersApi/Entities/AppContext.cs
@@ -1,6 +1,9 @@
-﻿namespace FoodOrdersApi.Entities
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FoodOrdersApi.Entities
 {
-    public class AppContext
+    public class AppContext : DbContext
     {
+        public AppContext(DbContextOptions<AppContext> options) : base(options) { }
     }
 }
