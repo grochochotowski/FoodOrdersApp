ffdf3170c74c8ac89b27b4cfb6b8b04636118ed5
grochochotowski <g.m.rochowski@gmail.com>
2024-03-15 15:34:08 +0100

    create basae controller for user

---
+++ FoodOrdersApi/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodOrdersApi.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+    }
+}
