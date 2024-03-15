798a0bdec8c37873197e8fff1c606641856e9877
grochochotowski <g.m.rochowski@gmail.com>
2024-03-15 14:57:48 +0100

    add user to individual order

--- FoodOrdersApi/Entities/IndividualOrder.cs
+++ FoodOrdersApi/Entities/IndividualOrder.cs
@@ -9,11 +9,15 @@ namespace FoodOrdersApi.Entities
 
         [Required(ErrorMessage = "Individual order has to be connected in bigger order")]
         public int OrderId { get; set; }
+        
+        [Required(ErrorMessage = "User is required")]
+        public int UserId { get; set; }
 
         public string? Notes { get; set; }
 
 
-        public virtual ICollection<Meal>? Meals { get; set; }
         public virtual Order Order { get; set; }
+        public virtual User User { get; set; }
+        public virtual ICollection<Meal>? Meals { get; set; }
     }
 }
