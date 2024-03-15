f4188b3a3972ade176a051f5fcd5d9727b3185a3
grochochotowski <g.m.rochowski@gmail.com>
2024-03-15 13:51:09 +0100

    create db context

--- FoodOrdersApi/Entities/AppDbContext.cs
+++ FoodOrdersApi/Entities/AppDbContext.cs
@@ -5,5 +5,15 @@ namespace FoodOrdersApi.Entities
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Meal> Meals { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
