e3c5fecf239f140138b15ce09e8a8a5dba8d6cb7
grochochotowski <g.m.rochowski@gmail.com>
2024-03-15 14:01:35 +0100

    create user entity

--- FoodOrdersApi/Entities/User.cs
+++ FoodOrdersApi/Entities/User.cs
@@ -1,6 +1,18 @@
-﻿namespace FoodOrdersApi.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodOrdersApi.Entities
 {
     public class User
     {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="First name is requierd")]
+        public string FirstName { get; set; }
+        public string? SecondName { get; set; }
+        [Required(ErrorMessage = "Last name is requierd")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Email is requierd")]
+        public string Email { get; set; }
+        public string? Note { get; set; }
     }
 }
