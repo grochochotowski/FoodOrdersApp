9f16df141f16adfec3e11a206e0031696da79a60
grochochotowski <g.m.rochowski@gmail.com>
2024-03-15 13:58:02 +0100

    create restaurant entity

--- FoodOrdersApi/Entities/Restaurant.cs
+++ FoodOrdersApi/Entities/Restaurant.cs
@@ -1,6 +1,15 @@
-﻿namespace FoodOrdersApi.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodOrdersApi.Entities
 {
     public class Restaurant
     {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name of the restaurant is required")]
+        [Display(Name = "Restaurant Name")]
+        public string Name { get; set; }
+        [Display(Name = "Restaurant Description")]
+        public string? Description { get; set; }
     }
 }
