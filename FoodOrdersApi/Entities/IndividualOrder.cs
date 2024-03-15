66354d28adb6fc118fdbf447424959643d81267c
grochochotowski <g.m.rochowski@gmail.com>
2024-03-15 14:54:39 +0100

    create individual order entity

---
+++ FoodOrdersApi/Entities/IndividualOrder.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodOrdersApi.Entities
+{
+    public class IndividualOrder
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Individual order has to be connected in bigger order")]
+        public int OrderId { get; set; }
+
+        public string? Notes { get; set; }
+
+
+        public virtual ICollection<Meal>? Meals { get; set; }
+        public virtual Order Order { get; set; }
+    }
+}
