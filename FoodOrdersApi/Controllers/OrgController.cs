192430fb24c645e2a830a1901ad3ef2383e67262
grochochotowski <g.m.rochowski@gmail.com>
2024-03-15 16:01:02 +0100

    create org controller

---
+++ FoodOrdersApi/Controllers/OrgController.cs
@@ -0,0 +1,26 @@
+﻿using FoodOrdersApi.Models;
+using FoodOrdersApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodOrdersApi.Controllers
+{
+    [Route("api/org")]
+    [ApiController]
+    public class OrgController : ControllerBase
+    {
+        private readonly IOrgService _orgService;
+
+        public OrgController(IOrgService orgService)
+        {
+            _orgService = orgService;
+        }
+
+
+        [HttpPost("create")]
+        public ActionResult Create([FromBody] CreateOrgDto dto)
+        {
+            var orgId = _orgService.Create(dto);
+            return Created($"{orgId}", null);
+        }
+    }
+}
