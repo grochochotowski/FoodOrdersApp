5ea9b334f41580281ee8a4845ed13f3d7024cfc4
grochochotowski <g.m.rochowski@gmail.com>
2024-03-15 14:07:06 +0100

    create organization entity

---
+++ FoodOrdersApi/Entities/Organization.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodOrdersApi.Entities
+{
+    public class Organization
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        public string? Note { get; set; }
+    }
+}
