32ebfe8c16c9efdfa4731148d158e09ad6632bda
grochochotowski <g.m.rochowski@gmail.com>
2024-03-18 23:45:45 +0100

    migrate database

--- FoodOrdersApi/Migrations/20240318224521_addAddressToCart.Designer.cs
+++ FoodOrdersApi/Migrations/20240318224521_addAddressToCart.Designer.cs
@@ -4,6 +4,7 @@ using FoodOrdersApi.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FoodOrdersApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240318224521_addAddressToCart")]
+    partial class addAddressToCart
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FoodOrdersApi/Migrations/20240318224521_addAddressToCart.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodOrdersApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class addAddressToCart : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Restaurants_Addresses_AddressId",
+                table: "Restaurants");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Restaurants_AddressId",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "AddressId",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AddressId",
+                table: "Carts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carts_AddressId",
+                table: "Carts",
+                column: "AddressId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_Addresses_AddressId",
+                table: "Carts",
+                column: "AddressId",
+                principalTable: "Addresses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_Addresses_AddressId",
+                table: "Carts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Carts_AddressId",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "AddressId",
+                table: "Carts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AddressId",
+                table: "Restaurants",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Restaurants_AddressId",
+                table: "Restaurants",
+                column: "AddressId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Restaurants_Addresses_AddressId",
+                table: "Restaurants",
+                column: "AddressId",
+                principalTable: "Addresses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
