da7e8330a1e224692fe80e6cbfe2f856636cf4db
grochochotowski <g.m.rochowski@gmail.com>
2024-03-18 23:33:46 +0100

    create cart dtos

---
+++ FoodOrdersApi/Models/Cart/CartDto.cs
@@ -0,0 +1,20 @@
+﻿using FoodOrdersApi.Models.Address;
+using FoodOrdersApi.Models.Restaurant;
+
+namespace FoodOrdersApi.Models.Cart
+{
+    public class CartDto
+    {
+        public int Id { get; set; }
+        public double MinPrice { get; set; }
+        public double DeliveryPrice { get; set; }
+        public double FreeDeliveryMinPrice { get; set; }
+        public string PhoneNumber { get; set; }
+        public string BankAccountNumber { get; set; }
+        public string? Note { get; set; }
+        public int RestaurantId { get; set; }
+        public RestaurantDto Restaurant { get; set; }
+        public int AddressId { get; set; }
+        public AddressDto Address { get; set; }
+    }
+}
---
+++ FoodOrdersApi/Models/Cart/CreateCartDto.cs
@@ -0,0 +1,20 @@
+﻿using FoodOrdersApi.Models.Address;
+using FoodOrdersApi.Models.Restaurant;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodOrdersApi.Models.Cart
+{
+    public class CreateCartDto
+    {
+        public double MinPrice { get; set; }
+        public double DeliveryPrice { get; set; }
+        public double FreeDeliveryMinPrice { get; set; }
+        public string PhoneNumber { get; set; }
+        public string BankAccountNumber { get; set; }
+        public string? Note { get; set; }
+
+
+        public int RestaurantId { get; set; }
+        public int AddressId { get; set; }
+    }
+}
