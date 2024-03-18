7fc9ede750f50f411bdaa0c28735b10f34ed1f09
grochochotowski <g.m.rochowski@gmail.com>
2024-03-18 18:46:36 +0100

    create new controller files

---
+++ FoodOrdersApi/Controllers/OrgController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FoodOrdersApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrgController : ControllerBase
+    {
+        // GET: api/<OrgController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<OrgController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<OrgController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<OrgController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<OrgController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
