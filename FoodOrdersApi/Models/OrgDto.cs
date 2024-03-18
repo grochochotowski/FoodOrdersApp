7c59890848147c7b026f5c402f07b2dfba8b63d5
grochochotowski <g.m.rochowski@gmail.com>
2024-03-18 18:58:39 +0100

    create model for orgdto

---
+++ FoodOrdersApi/Models/OrgDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodOrdersApi.Models
+{
+    public class OrgDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Note { get; set; }
+    }
+}
