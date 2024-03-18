99bd931871b664a15ca8db811d1ced486d71e2bd
grochochotowski <g.m.rochowski@gmail.com>
2024-03-18 23:41:06 +0100

    create order dtos

---
+++ FoodOrdersApi/Models/Order/CreateOrderDto.cs
@@ -0,0 +1,14 @@
+﻿using FoodOrdersApi.Entities.Objects;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodOrdersApi.Models.Order
+{
+    public class CreateOrderDto
+    {
+        public string? Notes { get; set; }
+
+
+        public int CartId { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ FoodOrdersApi/Models/Order/OrderDto.cs
@@ -0,0 +1,22 @@
+﻿using FoodOrdersApi.Entities.Objects;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodOrdersApi.Models.Order
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public int TotalPrice { get; set; }
+        public string? Notes { get; set; }
+
+
+        public int CartId { get; set; }
+
+
+        public int UserId { get; set; }
+        public virtual UserDto User { get; set; }
+
+
+        public virtual ICollection<Meal>? Meals { get; set; }
+    }
+}
