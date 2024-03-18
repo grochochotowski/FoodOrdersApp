f4d6eb10ff2a7abb2d077fcef7bacc0a190cf3ef
grochochotowski <g.m.rochowski@gmail.com>
2024-03-19 00:12:48 +0100

    Auto

--- FoodOrdersApi/obj/Debug/net8.0/FoodOrdersApi.AssemblyInfo.cs
+++ FoodOrdersApi/obj/Debug/net8.0/FoodOrdersApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodOrdersApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+540a879d0b3ad9211346059f09c307fcc60c1f7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+945972d803e60a2fc4378d13f41ff78e09a24eaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodOrdersApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodOrdersApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
