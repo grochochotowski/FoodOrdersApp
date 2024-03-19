1e152e67583c9e02a2921a3f2497216cf1ac3efa
grochochotowski <g.m.rochowski@gmail.com>
2024-03-19 08:54:28 +0100

    cop address service to user service

--- FoodOrdersApi/Services/UserService.cs
+++ FoodOrdersApi/Services/UserService.cs
@@ -6,30 +6,93 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FoodOrdersApi.Services
 {
-    public interface IUserService
+    public interface IAddressService
     {
-        int Create(CreateUserDto dto);
+        int Create(CreateAddressDto dto);
+        IEnumerable<AddressDto> GetAll();
+        AddressDto GetByID(int id);
+        int Update(int id, CreateAddressDto dto);
+        int Delete(int id);
     }
 
-    public class UserService : IUserService
+    public class AddressService : IAddressService
     {
         private readonly AppDbContext _context;
         private readonly IMapper _mapper;
 
-        public UserService(AppDbContext context, IMapper mapper)
+        public AddressService(AppDbContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
         }
 
 
-        public int Create(CreateUserDto dto)
+
+        // Create new address
+        public int Create(CreateAddressDto dto)
         {
-            var user = _mapper.Map<User>(dto);
-            _context.Users.Add(user);
+            var address = _mapper.Map<Address>(dto);
+            _context.Addresses.Add(address);
             _context.SaveChanges();
 
-            return user.Id;
+            return address.Id;
         }
+
+
+        // Get all address
+        public IEnumerable<AddressDto> GetAll()
+        {
+            var addresses = _context.Addresses.ToList();
+            var addressDtos = _mapper.Map<List<AddressDto>>(addresses);
+
+            return addressDtos;
+        }
+
+
+        // Get address by ID
+        public AddressDto GetByID(int id)
+        {
+            var address = _context.Addresses.FirstOrDefault(o => o.Id == id);
+            if (address == null) return null;
+
+            var addressDto = _mapper.Map<AddressDto>(address);
+
+
+            return addressDto;
+        }
+
+
+        // Update address with id
+        public int Update(int id, CreateAddressDto dto)
+        {
+            var address = _context.Addresses
+                .FirstOrDefault(o => o.Id == id);
+
+            if (address == null) return 0;
+
+            address.Country = dto.Country;
+            address.City = dto.City;
+            address.Street = dto.Street;
+            address.Building = dto.Building;
+            address.Premises = dto.Premises;
+
+            _context.SaveChanges();
+            return address.Id;
+        }
+
+
+        // Update address with id
+        public int Delete(int id)
+        {
+            var address = _context.Addresses
+                .FirstOrDefault(o => o.Id == id);
+
+            if (address == null) return 0;
+
+            _context.Addresses.Remove(address);
+            _context.SaveChanges();
+            return 1;
+        }
+
     }
 }
