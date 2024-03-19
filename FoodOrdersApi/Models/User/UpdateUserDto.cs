d560f0fd6eb40ef98fb468f0361066390339dfe0
grochochotowski <g.m.rochowski@gmail.com>
2024-03-19 09:18:12 +0100

    create dto for updating user

---
+++ FoodOrdersApi/Models/User/UpdateUserDto.cs
@@ -0,0 +1,14 @@
+﻿namespace FoodOrdersApi.Models.User
+{
+    public class UpdateUserDto
+    {
+        public string? FirstName { get; set; }
+        public string? SecondName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Note { get; set; }
+
+
+        public int? OrganizationId { get; set; }
+    }
+}
