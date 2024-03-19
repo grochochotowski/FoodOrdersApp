cfed40e0e4143fe0c87042b9fd5ac1fbc1b8d1cd
grochochotowski <g.m.rochowski@gmail.com>
2024-03-19 09:00:46 +0100

    refactor

--- FoodOrdersApi/Controllers/UserController.cs
+++ FoodOrdersApi/Controllers/UserController.cs
@@ -2,8 +2,6 @@
 using FoodOrdersApi.Services;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace FoodOrdersApi.Controllers
 {
     [Route("api/user")]
