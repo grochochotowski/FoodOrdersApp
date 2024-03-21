63b89ba228921e6fbf821f0878daf86120fdb6e0
grochochotowski <g.m.rochowski@gmail.com>
2024-03-21 10:14:03 +0100

    create model for adding meals

---
+++ FoodOrdersApi/Models/Order/AddOrderMeal.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace FoodOrdersApi.Models.Order
+{
+    public class AddOrderMeal
+    {
+        public List<int> meals { get; set; }
+    }
+}
