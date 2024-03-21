13ae94465c3c45aa5cbcf531ed8eff2da390a688
grochochotowski <g.m.rochowski@gmail.com>
2024-03-21 09:16:24 +0100

    migrate db

---
+++ FoodOrdersApi/Migrations/20240321081435_MealPrice.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodOrdersApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class MealPrice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Price",
+                table: "Meals",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Meals");
+        }
+    }
+}
--- FoodOrdersApi/obj/Debug/net8.0/FoodOrdersApi.AssemblyInfo.cs
+++ FoodOrdersApi/obj/Debug/net8.0/FoodOrdersApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodOrdersApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4d51442b0a887e2b8b2d2fc2cd68df558277bda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84976a19f10c8e4859c5c63e4af172ba328180d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodOrdersApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodOrdersApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
