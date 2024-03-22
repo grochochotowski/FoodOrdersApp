0e7d45897acea3edc1045ae59b9dc063f990efb7
grochochotowski <g.m.rochowski@gmail.com>
2024-03-22 13:15:59 +0100

    add db seeder to auto migrate db

---
+++ FoodOrdersApi/DBSeeder.cs
@@ -0,0 +1,27 @@
+﻿using FoodOrdersApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace shop_system
+{
+    public class DBSeeder
+    {
+        private readonly AppDbContext _context;
+
+        public DBSeeder(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Seed()
+        {
+            if (_context.Database.CanConnect())
+            {
+                var pendidngMigrations = _context.Database.GetPendingMigrations();
+                if (pendidngMigrations != null && pendidngMigrations.Any())
+                {
+                    _context.Database.Migrate();
+                }
+            }
+        }
+    }
+}
--- FoodOrdersApi/obj/Debug/net8.0/FoodOrdersApi.AssemblyInfo.cs
+++ FoodOrdersApi/obj/Debug/net8.0/FoodOrdersApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodOrdersApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bf1cf059ec3e30618a5b4072b1af1ea0dcae468")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfb80806261832323a68c21182744df63313434f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodOrdersApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodOrdersApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
