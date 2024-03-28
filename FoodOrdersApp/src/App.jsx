11a07a4b1bd12928137ed9d39d3b4e60d81b2c4d
grochochotowski <g.m.rochowski@gmail.com>
2024-03-28 22:56:36 +0100

    use react router to test

--- FoodOrdersApp/src/App.jsx
+++ FoodOrdersApp/src/App.jsx
@@ -1,20 +1,32 @@
 // MAIN IMPORTS
+import { Suspense, lazy } from "react";
+import { Routes, Route } from "react-router-dom";
 
 // STYLES
-import './styles/App.css'
+import "./styles/App.css";
 
 // COMPONENTS
-import Fallback from './components/Fallback';
+import Fallback from "./components/Fallback";
 import NavBar from "./components/NavBar";
 
 // PAGES
-
+const Test = lazy(() => import("./pages/Test.jsx"));
 
 export default function App() {
-
   return (
-    <>
-      <NavBar />
-    </>
-  )
+    <div className="main-container">
+
+        <NavBar />
+
+        <Routes>
+            <Route path="/test" element={
+                <Suspense fallback={<Fallback />}>
+                    <Test />
+                </Suspense>
+            }/>
+        </Routes>
+
+
+    </div>
+  );
 }
