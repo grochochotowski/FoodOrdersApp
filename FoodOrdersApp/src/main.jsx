5533f14423f7c862137595c00d3b38958d68b4b0
grochochotowski <g.m.rochowski@gmail.com>
2024-03-28 23:02:13 +0100

    change router to browserrouter

--- FoodOrdersApp/src/main.jsx
+++ FoodOrdersApp/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './App.jsx'
 import './styles/index.css'
