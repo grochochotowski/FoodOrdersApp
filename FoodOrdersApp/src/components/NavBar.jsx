49b51805c828c3c81d55ed696470875579b96ffe
grochochotowski <g.m.rochowski@gmail.com>
2024-03-28 15:16:39 +0100

    create base structure of navbar

--- FoodOrdersApp/src/components/NavBar.jsx
+++ FoodOrdersApp/src/components/NavBar.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav className='nav-container'>
+        <div className="nav-box">
+            <div className="logo">
+                <h1>FoodOrdersApp</h1>
+            </div>
+            <div className="menu">
+                <ul>
+                    <li>Home</li>
+                    <li>Carts</li>
+                    <li>Manage</li>
+                    <li>About</li>
+                </ul>
+            </div>
+        </div>
+    </nav>
   )
 }
