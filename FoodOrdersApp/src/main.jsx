71999d4036ce76145fc2fcab083668eed3805ecd
grochochotowski <g.m.rochowski@gmail.com>
2024-03-28 22:52:34 +0100

    use ract router

--- FoodOrdersApp/src/main.jsx
+++ FoodOrdersApp/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Router } from 'react-router-dom'
 import App from './App.jsx'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Router>
+            <App />
+        </Router>
+    </React.StrictMode>,
 )
