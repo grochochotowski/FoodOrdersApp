667e676d58837640e429ee7b9941a0df91f8558a
grochochotowski <g.m.rochowski@gmail.com>
2024-03-28 15:29:24 +0100

    use Link for router

--- FoodOrdersApp/src/components/NavBar.jsx
+++ FoodOrdersApp/src/components/NavBar.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 export default function NavBar() {
   return (
-    <nav className='nav-container'>
-        <div className="nav-box">
+    <nav className='container'>
+        <div className="box">
             <div className="logo">
                 <h1>FoodOrdersApp</h1>
             </div>
             <div className="menu">
                 <ul>
-                    <li>Home</li>
-                    <li>Carts</li>
-                    <li>Manage</li>
-                    <li>About</li>
+                    <li>
+                        <Link to="example">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="example">Carts</Link>
+                    </li>
+                    <li>
+                        <Link to="example">Manage</Link>
+                    </li>
+                    <li>
+                        <Link to="example">About</Link>
+                    </li>
                 </ul>
             </div>
         </div>
