c288965ebacb62e950fc3258e6fe57cd1f9caf8b
grochochotowski <g.m.rochowski@gmail.com>
2024-03-29 10:03:27 +0100

    create bas styling of log in box

--- FoodOrdersApp/src/pages/LogIn.jsx
+++ FoodOrdersApp/src/pages/LogIn.jsx
@@ -10,23 +10,25 @@ export default function LogIn() {
     }
 
     return (
-      <main className="container">
-          <h1>Log in to your account</h1>
-          <div className="form">
-              <div className="login">
-                  <label htmlFor="login">Login:</label>
-                  <input type="text" id="login"/>
-              </div>
-              <label htmlFor="password">Password:</label>
-              <div className="password-input">
-                  <input type={isPasswordShown ? "text" : "password"} id="password"/>
-                  {isPasswordShown
-                  ? <i className="fa-regular fa-eye" onClick={() => changePasswordVisibility()}></i>
-                  : <i className="fa-regular fa-eye-slash" onClick={() => changePasswordVisibility()}></i>
-                  }
-              </div>
-          </div>
-      </main>
+        <main className="container">
+            <div className="box">
+                <h1>Log in to your account</h1>
+                <div className="form">
+                    <div className="login">
+                        <label htmlFor="login">Login:</label>
+                        <input type="text" id="login"/>
+                    </div>
+                    <label htmlFor="password">Password:</label>
+                    <div className="password-input">
+                        <input type={isPasswordShown ? "text" : "password"} id="password"/>
+                        {isPasswordShown
+                        ? <i className="fa-regular fa-eye" onClick={() => changePasswordVisibility()}></i>
+                        : <i className="fa-regular fa-eye-slash" onClick={() => changePasswordVisibility()}></i>
+                        }
+                    </div>
+                </div>
+            </div>
+        </main>
     )
-  }
+}
   
