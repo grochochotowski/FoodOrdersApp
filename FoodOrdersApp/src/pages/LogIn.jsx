35c5967ff9af7a9aa5e8fd547b2a0c52f28a261d
grochochotowski <g.m.rochowski@gmail.com>
2024-03-29 09:45:22 +0100

    bing back login page

---
+++ FoodOrdersApp/src/pages/LogIn.jsx
@@ -0,0 +1,20 @@
+export default function LogIn() {
+    return (
+      <main className="container">
+          <h1>Log in to your account</h1>
+          <div className="form">
+              <div className="login">
+                  <label htmlFor="login">Login:</label>
+                  <input type="text" id="login"/>
+              </div>
+              <label htmlFor="password">Password:</label>
+              <div className="password-input">
+                  <input type="password" id="password"/>
+                  <i className="fa-regular fa-eye"></i>
+                  <i className="fa-regular fa-eye-slash"></i>
+              </div>
+          </div>
+      </main>
+    )
+  }
+  
