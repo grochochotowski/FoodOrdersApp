b57626e2e06d7f6b5cd38a89c1f9ba504282207a
grochochotowski <g.m.rochowski@gmail.com>
2024-03-29 09:53:44 +0100

    handle changing password visibility

--- FoodOrdersApp/src/pages/LogIn.jsx
+++ FoodOrdersApp/src/pages/LogIn.jsx
@@ -1,4 +1,14 @@
+import { useState } from "react"
+import "../styles/logIn.css"
+
 export default function LogIn() {
+
+    const [isPasswordShown, setIsPasswordShown] = useState(false)
+
+    function changePasswordVisibility() {
+        setIsPasswordShown(prev => !prev)
+    }
+
     return (
       <main className="container">
           <h1>Log in to your account</h1>
@@ -9,9 +19,11 @@ export default function LogIn() {
               </div>
               <label htmlFor="password">Password:</label>
               <div className="password-input">
-                  <input type="password" id="password"/>
-                  <i className="fa-regular fa-eye"></i>
-                  <i className="fa-regular fa-eye-slash"></i>
+                  <input type={isPasswordShown ? "text" : "password"} id="password"/>
+                  {isPasswordShown
+                  ? <i className="fa-regular fa-eye" onClick={() => changePasswordVisibility()}></i>
+                  : <i className="fa-regular fa-eye-slash" onClick={() => changePasswordVisibility()}></i>
+                  }
               </div>
           </div>
       </main>
