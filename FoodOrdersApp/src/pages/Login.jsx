26b001dac646c41112c33eee993f06b33cbb0e17
grochochotowski <g.m.rochowski@gmail.com>
2024-03-29 09:43:05 +0100

    use font-awesome icons for displaying if passwod is shown or not

--- FoodOrdersApp/src/pages/Login.jsx
+++ FoodOrdersApp/src/pages/Login.jsx
@@ -1,5 +1,19 @@
 export default function Login() {
   return (
-    <div>Login</div>
+    <main className="container">
+        <h1>Log in to your account</h1>
+        <div className="form">
+            <div className="login">
+                <label htmlFor="login">Login:</label>
+                <input type="text" id="login"/>
+            </div>
+            <label htmlFor="password">Password:</label>
+            <div className="password-input">
+                <input type="password" id="password"/>
+                <i className="fa-regular fa-eye"></i>
+                <i className="fa-regular fa-eye-slash"></i>
+            </div>
+        </div>
+    </main>
   )
 }
