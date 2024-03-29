db15c2aa8137634dd682b6ffbd18faf4859539f4
grochochotowski <g.m.rochowski@gmail.com>
2024-03-29 11:41:37 +0100

    create filter section

--- FoodOrdersApp/src/pages/Carts.jsx
+++ FoodOrdersApp/src/pages/Carts.jsx
@@ -1,5 +1,25 @@
 export default function Carts() {
   return (
-    <div>Carts</div>
+    <div className="container">
+        <section className="container">
+            <h1>Carts</h1>
+            <div className="filter">
+                <h3>Filters</h3>
+                <div className="restaurant-filter">
+                    <label htmlFor="restaurant-input">
+                        <h5>Restaurant:</h5>
+                    </label>
+                    <input type="text" id="restaurant-input"/>
+                </div>
+                <div className="organization-filter">
+                    <label htmlFor="organization-input">
+                        <h5>Organization:</h5>
+                    </label>
+                    <input type="text" id="organization-input"/>
+                </div>
+                <button>Filter</button>
+            </div>
+        </section>
+    </div>
   )
 }
