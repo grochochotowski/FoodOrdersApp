5be0c40b1508e948d4d590414952ecce6e0b68d5
grochochotowski <g.m.rochowski@gmail.com>
2024-04-01 00:18:09 +0200

    create path on carts page

--- FoodOrdersApp/src/pages/Carts/CartsNew.jsx
+++ FoodOrdersApp/src/pages/Carts/CartsNew.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 export default function CartsNew() {
+
+    const navigate = useNavigate();
+
   return (
-    <div>CartsNew</div>
+    <div className="container">
+            <section className="box">
+                <h1>Carts</h1>
+                <div className="path">
+                    <p onClick={() => {navigate("/carts")}}>Carts</p>
+                    <p>&gt;&gt;&gt;</p>
+                    <p onClick={() => {navigate("/carts/new")}}>New cart</p>
+                </div>
+            </section>
+        </div>
   )
 }
