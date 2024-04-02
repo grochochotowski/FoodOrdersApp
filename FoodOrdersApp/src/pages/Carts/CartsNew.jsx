c3823a570fa299ce25110124e3228391085483d1
grochochotowski <g.m.rochowski@gmail.com>
2024-04-02 19:52:48 +0200

    change file structure of add new cart

--- FoodOrdersApp/src/pages/Carts/CartsNew.jsx
+++ FoodOrdersApp/src/pages/Carts/CartsNew.jsx
@@ -18,65 +18,87 @@ export default function CartsNew() {
                     <div className="line">
                         <div className="layer">
 
-                            <label htmlFor="restaurant">Restaurant:</label>
-                            <select name="restaurant" id="restaurant">
-                                <option value="r1">Restaurant 1</option>
-                                <option value="r2">Restaurant 2</option>
-                                <option value="r3">Restaurant 3</option>
-                                <option value="r4">Restaurant 4</option>
-                                <option value="r5">Restaurant 5</option>
-                                <option value="r6">Restaurant 6</option>
-                            </select>
-                            
-                            <label htmlFor="organization">Organization:</label>
-                            <select name="restaurant" id="restaurant">
-                                <option value="o1">Organization 1</option>
-                                <option value="o2">Organization 2</option>
-                                <option value="o3">Organization 3</option>
-                                <option value="o4">Organization 4</option>
-                                <option value="o5">Organization 5</option>
-                                <option value="o6">Organization 6</option>
-                            </select>
+                            <div className="input-container">
+                                <label htmlFor="restaurant">Restaurant:</label>
+                                <select name="restaurant" id="restaurant">
+                                    <option value="r1">Restaurant 1</option>
+                                    <option value="r2">Restaurant 2</option>
+                                    <option value="r3">Restaurant 3</option>
+                                    <option value="r4">Restaurant 4</option>
+                                    <option value="r5">Restaurant 5</option>
+                                    <option value="r6">Restaurant 6</option>
+                                </select>
+                            </div>
+
+                            <div className="input-container">
+                                <label htmlFor="organization">Organization:</label>
+                                <select name="restaurant" id="restaurant">
+                                    <option value="o1">Organization 1</option>
+                                    <option value="o2">Organization 2</option>
+                                    <option value="o3">Organization 3</option>
+                                    <option value="o4">Organization 4</option>
+                                    <option value="o5">Organization 5</option>
+                                    <option value="o6">Organization 6</option>
+                                </select>
+                            </div>
 
                         </div>
                     </div>
                     <div className="line top-bottom">
                         <div className="layer top">
 
-                            <label htmlFor="bank-account-number">Bank account number:</label>
-                            <input type="text" />
+                            <div className="input-container">
+                                <label htmlFor="bank-account-number">Bank account number:</label>
+                                <input type="text" />
+                            </div>
 
-                            <label htmlFor="phone-number">Phone number:</label>
-                            <input type="text" />
+                            <div className="input-container">
+                                <label htmlFor="phone-number">Phone number:</label>
+                                <input type="text" />
+                            </div>
 
                         </div>
                         <div className="layer bottom">
 
-                            <label htmlFor="city">City:</label>
-                            <input type="text" />
+                            <div className="input-container">
+                                <label htmlFor="city">City:</label>
+                                <input type="text" />
+                            </div>
 
-                            <label htmlFor="street">Street:</label>
-                            <input type="text" />
+                            <div className="input-container">
+                                <label htmlFor="street">Street:</label>
+                                <input type="text" />
+                            </div>
 
-                            <label htmlFor="building">Building:</label>
-                            <input type="text" />
+                            <div className="input-container">
+                                <label htmlFor="building">Building:</label>
+                                <input type="text" />
+                            </div>
 
-                            <label htmlFor="premises">Premises:</label>
-                            <input type="text" />
+                            <div className="input-container">
+                                <label htmlFor="premises">Premises:</label>
+                                <input type="text" />
+                            </div>
 
                         </div>
                     </div>
                     <div className="line left-right">
                         <div className="layer left">
 
-                            <label htmlFor="min-price">Minimum cart price:</label>
-                            <input type="text" />
+                            <div className="input-container">
+                                <label htmlFor="min-price">Minimum cart price:</label>
+                                <input type="text" />
+                            </div>
 
-                            <label htmlFor="delivery-price">Delivery price:</label>
-                            <input type="text" />
+                            <div className="input-container">
+                                <label htmlFor="delivery-price">Delivery price:</label>
+                                <input type="text" />
+                            </div>
 
-                            <label htmlFor="free-delivery-price">Free delivery price:</label>
-                            <input type="text" />
+                            <div className="input-container">
+                                <label htmlFor="free-delivery-price">Free delivery price:</label>
+                                <input type="text" />
+                            </div>
 
                         </div>
                         <div className="layer right">
