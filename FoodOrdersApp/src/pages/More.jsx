c93a5eccb346025e3503654be882dbf53dc02e4e
grochochotowski <g.m.rochowski@gmail.com>
2024-04-05 00:14:08 +0200

    change link to navigate

--- FoodOrdersApp/src/pages/More.jsx
+++ FoodOrdersApp/src/pages/More.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
-import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 import "../styles/more.css"
 import "../styles/index.css"
 import "../styles/App.css"
 
 export default function More() {
+
+    const navigate = useNavigate()
+
     return (
         <div className="container">
             <section className="box">
                 <h1>More pages</h1>
                 <div className="pages">
-                    <Link to="/restaurants">
+                    <div className="page-container" onClick={navigate("/restaurants")}>
                         <h2>Restaurants</h2>
-                    </Link>
+                    </div>
                     
-                    <Link to="/users">
+                    <div className="page-container" onClick={navigate("/users")}>
                         <h2>Users</h2>
-                    </Link>
+                    </div>
                     
-                    <Link to="/organizations">
+                    <div className="page-container" onClick={navigate("/organizations")}>
                         <h2>Organizations</h2>
-                    </Link>
+                    </div>
                 </div>
             </section>
         </div>
