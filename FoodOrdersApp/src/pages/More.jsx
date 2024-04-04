88af271bd842153592bd07017f4603c3a519961a
grochochotowski <g.m.rochowski@gmail.com>
2024-04-05 00:21:27 +0200

    fix routing

--- FoodOrdersApp/src/pages/More.jsx
+++ FoodOrdersApp/src/pages/More.jsx
@@ -14,15 +14,15 @@ export default function More() {
             <section className="box">
                 <h1>More pages</h1>
                 <div className="pages">
-                    <div className="page-container" onClick={navigate("/restaurants")}>
+                    <div className="page-container" onClick={() => {navigate("/restaurants")}}>
                         <h3>Restaurants</h3>
                     </div>
                     
-                    <div className="page-container" onClick={navigate("/users")}>
+                    <div className="page-container" onClick={() => {navigate("/users")}}>
                         <h3>Users</h3>
                     </div>
                     
-                    <div className="page-container" onClick={navigate("/organizations")}>
+                    <div className="page-container" onClick={() => {navigate("/organizations")}}>
                         <h3>Organizations</h3>
                     </div>
                 </div>
