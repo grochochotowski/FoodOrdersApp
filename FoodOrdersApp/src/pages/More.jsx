f4f6f17b15f7cbf1621f9f63b2c36064ffa09cb2
grochochotowski <g.m.rochowski@gmail.com>
2024-04-05 00:09:06 +0200

    create more structure

--- FoodOrdersApp/src/pages/More.jsx
+++ FoodOrdersApp/src/pages/More.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+
+import "../styles/more.css"
+import "../styles/index.css"
+import "../styles/App.css"
 
 export default function More() {
     return (
-        <div>More</div>
+        <div className="container">
+            <section className="box">
+                <h1>More pages</h1>
+                <div className="pages">
+                    <Link to="/restaurants">
+                        <h2>Restaurants</h2>
+                    </Link>
+                    
+                    <Link to="/users">
+                        <h2>Users</h2>
+                    </Link>
+                    
+                    <Link to="/organizations">
+                        <h2>Organizations</h2>
+                    </Link>
+                </div>
+            </section>
+        </div>
     )
 }
