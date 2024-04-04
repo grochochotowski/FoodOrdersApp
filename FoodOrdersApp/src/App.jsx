5e0475a30c72c95c6a626948efddd8c5da9a77a4
grochochotowski <g.m.rochowski@gmail.com>
2024-04-05 00:31:38 +0200

    create routing for more pages

--- FoodOrdersApp/src/App.jsx
+++ FoodOrdersApp/src/App.jsx
@@ -24,6 +24,20 @@ const OrdersEdit = lazy(() => import("./pages/Orders/OrdersEdit.jsx"));
 
 const More = lazy(() => import("./pages/More.jsx"));
 
+const Restaurants = lazy(() => import("./pages/Restaurants/Restaurants.jsx"));
+const RestaurantsNew = lazy(() => import("./pages/Restaurants/RestaurantsNew.jsx"));
+const RestaurantsDetails = lazy(() => import("./pages/Restaurants/RestaurantsDetails.jsx"));
+const RestaurantsEdit = lazy(() => import("./pages/Restaurants/RestaurantsEdit.jsx"));
+
+const Users = lazy(() => import("./pages/Users/Users.jsx"));
+const UsersNew = lazy(() => import("./pages/Users/UsersNew.jsx"));
+const UsersDetails = lazy(() => import("./pages/Users/UsersDetails.jsx"));
+const UsersEdit = lazy(() => import("./pages/Users/UsersEdit.jsx"));
+
+const Organizations = lazy(() => import("./pages/Organizations/Organizations.jsx"));
+const OrganizationsNew = lazy(() => import("./pages/Organizations/OrganizationsNew.jsx"));
+const OrganizationsEdit = lazy(() => import("./pages/Organizations/OrganizationsEdit.jsx"));
+
 export default function App() {
   return (
     <div className="main-container">
@@ -86,6 +100,67 @@ export default function App() {
             <Route path="/more" element={
                 <Suspense fallback={<Fallback />}>
                     <More />
+                </Suspense>
+            }/>
+
+
+
+            <Route path="/restaurants" element={
+                <Suspense fallback={<Fallback />}>
+                    <Restaurants />
+                </Suspense>
+            }/><Route path="/restaurants/new" element={
+                <Suspense fallback={<Fallback />}>
+                    <RestaurantsNew />
+                </Suspense>
+            }/>
+            <Route path="/restaurants/details/:id" element={
+                <Suspense fallback={<Fallback />}>
+                    <RestaurantsDetails />
+                </Suspense>
+            }/>
+            <Route path="/restaurants/edit/:id" element={
+                <Suspense fallback={<Fallback />}>
+                    <RestaurantsEdit />
+                </Suspense>
+            }/>
+
+
+
+            <Route path="/users" element={
+                <Suspense fallback={<Fallback />}>
+                    <Users />
+                </Suspense>
+            }/><Route path="/users/new" element={
+                <Suspense fallback={<Fallback />}>
+                    <UsersNew />
+                </Suspense>
+            }/>
+            <Route path="/users/details/:id" element={
+                <Suspense fallback={<Fallback />}>
+                    <UsersDetails />
+                </Suspense>
+            }/>
+            <Route path="/users/edit/:id" element={
+                <Suspense fallback={<Fallback />}>
+                    <UsersEdit />
+                </Suspense>
+            }/>
+
+
+
+            <Route path="/organizations" element={
+                <Suspense fallback={<Fallback />}>
+                    <Organizations />
+                </Suspense>
+            }/><Route path="/organizations/new" element={
+                <Suspense fallback={<Fallback />}>
+                    <OrganizationsNew />
+                </Suspense>
+            }/>
+            <Route path="/organizations/edit/:id" element={
+                <Suspense fallback={<Fallback />}>
+                    <OrganizationsEdit />
                 </Suspense>
             }/>
 
