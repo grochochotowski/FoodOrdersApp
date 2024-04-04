f2a0a2bdc56bf5c577d0282e86cb7bd8c58bbab3
grochochotowski <g.m.rochowski@gmail.com>
2024-04-04 09:56:13 +0200

    create layout for details

---
+++ FoodOrdersApp/src/components/IndividualOrder.jsx
@@ -0,0 +1,5 @@
+export default function IndividualOrder({order}) {
+  return (
+    <div>IndividualOrder: {order.name}</div>
+  )
+}
--- FoodOrdersApp/src/pages/Carts/CartsDetails.jsx
+++ FoodOrdersApp/src/pages/Carts/CartsDetails.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 import { Link, useParams } from "react-router-dom"
 
+import IndividualOrder from "../../components/IndividualOrder";
+
+import "../../styles/carts.css"
+import "../../styles/index.css"
+import "../../styles/App.css"
+
 export default function CartsDetails() {
 
     const params = useParams();
@@ -29,12 +35,83 @@ export default function CartsDetails() {
             "name" : "McDonald's"
         },
     })
+    const [orders, setOrders] = useState([
+        {
+            "img" : "https://source.unsplash.com/random/200x200?sig=1",
+            "name" : "Name 1",
+            "price" : 5.99,
+            "positions" : 2
+        },
+        {
+            "img" : "https://source.unsplash.com/random/200x200?sig=2",
+            "name" : "Name 2",
+            "price" : 4.99,
+            "positions" : 3
+        },
+        {
+            "img" : "https://source.unsplash.com/random/200x200?sig=3",
+            "name" : "Name 3",
+            "price" : 3.99,
+            "positions" : 2
+        },
+        {
+            "img" : "https://source.unsplash.com/random/200x200?sig=4",
+            "name" : "Name 4",
+            "price" : 2.99,
+            "positions" : 5
+        },
+    ])
 
     return (
         <div className="container">
             <section className="box">
                 <h1>Cart &#40;{cartDetails.id}&#41; - {cartDetails.organization.name} - {cartDetails.restaurant.name}</h1>
-
+                <div className="info">
+                    <div className="left">
+                        <div className="line">
+                            <h5>Cost:</h5>
+                            <p>{cartDetails.totalCartPrice}/{cartDetails.minPrice} zł</p>
+                            <h5>Delivery:</h5>
+                            {
+                                cartDetails.deliveryPrice == 0
+                                ? <p>Free</p>
+                                : <p>
+                                    {cartDetails.deliveryPrice}
+                                    zł - {cartDetails.freeDeliveryPrice - cartDetails.totalCartPrice}zł
+                                    to free delivery
+                                </p>
+                            }
+                        </div>
+                        <div className="line">
+                            <h5>Address:</h5>
+                            <p>
+                                {cartDetails.address.country}
+                                , {cartDetails.address.city}
+                                , {cartDetails.address.street}
+                                &nbsp;{cartDetails.address.building}
+                                {cartDetails.address.premises && <>/{cartDetails.address.premises}</>}
+                            </p>
+                        </div>
+                        <div className="line">
+                            <h5>Phone number:</h5>
+                            <p>{cartDetails.phoneNumber}</p>
+                            <h5>Bank account number:</h5>
+                            <p>{cartDetails.bankAccountNumber}</p>
+                        </div>
+                        <div className="line">
+                            <h5>Notes:</h5>
+                            <p>{cartDetails.notes}</p>
+                        </div>
+                    </div>
+                    <div className="right">
+                        <div className="orders">
+                            <IndividualOrder order={orders[0]}/>
+                            <IndividualOrder order={orders[1]}/>
+                            <IndividualOrder order={orders[2]}/>
+                            <IndividualOrder order={orders[3]}/>
+                        </div>
+                    </div>
+                </div>
             </section>
         </div>
     )
