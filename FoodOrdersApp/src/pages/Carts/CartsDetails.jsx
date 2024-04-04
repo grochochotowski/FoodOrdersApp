fb2ac5b3dc8d0e8fbace470b1fc74b2ce9db17e3
grochochotowski <g.m.rochowski@gmail.com>
2024-04-04 09:18:32 +0200

    create base alyout or details page

--- FoodOrdersApp/src/pages/Carts/CartsDetails.jsx
+++ FoodOrdersApp/src/pages/Carts/CartsDetails.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import { Link, useParams } from "react-router-dom"
+
 export default function CartsDetails() {
-  return (
-    <div>CartsDetails</div>
-  )
+
+    const params = useParams();
+    console.log(params.id)
+
+    const [cartDetails, setCartDetails] = useState({
+        "id" : 1,
+        "minPrice": 19.99,
+        "totalCartPrice" : 19.99,
+        "deliveryPrice" : 0.00,
+        "organization" : {
+            "name" : "Organization 1"
+        },
+        "restaurant" : {
+            "name" : "McDonald's"
+        }
+    })
+
+    return (
+        <div className="container">
+            <section className="box">
+                <h1>Cart &#40;{cartDetails.id}&#41; - {cartDetails.organization.name} - {cartDetails.restaurant.name}</h1>
+
+            </section>
+        </div>
+    )
 }
