6bd53b8ede9cf881f4d5bc28d33fed0df62fcbab
grochochotowski <g.m.rochowski@gmail.com>
2024-04-09 12:57:36 +0200

    create pagedresult entity

---
+++ FoodOrdersApi/Entities/PagedResult.cs
@@ -0,0 +1,20 @@
+﻿namespace FoodOrdersApi.Entities
+{
+    public class PagedResult<T>
+    {
+        public List<T> Items { get; set; }
+        public int TotalPages { get; set; }
+        public int ItemFrom { get; set; }
+        public int ItemTo { get; set; }
+        public int totalItemsCount { get; set; }
+
+        public PagedResult(List<T> items, int totalCount, int page)
+        {
+            Items = items;
+            totalItemsCount = totalCount;
+            ItemFrom = 10 * (page - 1) + 1;
+            TotalPages = (int)Math.Ceiling( totalCount / (double)10 );
+        }
+
+    }
+}
