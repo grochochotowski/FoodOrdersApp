b694e8585aceb21ff71c1939abfdfb55042c5475
grochochotowski <g.m.rochowski@gmail.com>
2024-04-11 14:00:49 +0200

    migrate db with postiions in order

---
+++ FoodOrdersApi/Migrations/20240411115747_positionsInOrder.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodOrdersApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class positionsInOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "MealOrder");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Positions",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Positions",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Price",
+                table: "MealOrder",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
