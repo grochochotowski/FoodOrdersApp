ab2a159794ab08fe8cda0ddf4cc9682e50207a08
grochochotowski <g.m.rochowski@gmail.com>
2024-04-11 14:08:17 +0200

    remove example data

--- FoodOrdersApp/src/pages/Carts/CartsDetails.jsx
+++ FoodOrdersApp/src/pages/Carts/CartsDetails.jsx
@@ -12,87 +12,8 @@ export default function CartsDetails() {
     const params = useParams();
     const navigate = useNavigate();
 
-    const [cartDetails, setCartDetails] = useState({
-        "id" : 1,
-        "minPrice": 19.99,
-        "totalCartPrice" : 19.99,
-        "deliveryPrice" : 0.00,
-        "freeDeliveryPrice" : 19.99,
-        "phoneNumber": "123 456 789",
-        "bankAccountNumber": "12 3456 7890 1234 5678 9012 3456",
-        "notes": "Those are notes for order with id: 1",
-        "address": {
-            "country": "PL",
-            "city": "Bialystok",
-            "street": "Żurawia",
-            "building": "71",
-            "premises": "123"
-        },
-        "organization" : {
-            "name" : "Organization 1"
-        },
-        "restaurant" : {
-            "name" : "McDonald's"
-        },
-    })
-    const [orders, setOrders] = useState([
-        {
-            "id" : 1,
-            "img" : "https://source.unsplash.com/random/200x200?sig=1",
-            "name" : "Name 1",
-            "price" : 5.99,
-            "positions" : 2
-        },
-        {
-            "id" : 2,
-            "img" : "https://source.unsplash.com/random/200x200?sig=2",
-            "name" : "Name 2",
-            "price" : 4.99,
-            "positions" : 3
-        },
-        {
-            "id" : 3,
-            "img" : "https://source.unsplash.com/random/200x200?sig=3",
-            "name" : "Name 3",
-            "price" : 3.99,
-            "positions" : 2
-        },
-        {
-            "id" : 4,
-            "img" : "https://source.unsplash.com/random/200x200?sig=4",
-            "name" : "Name 4",
-            "price" : 2.99,
-            "positions" : 5
-        },
-        {
-            "id" : 5,
-            "img" : "https://source.unsplash.com/random/200x200?sig=5",
-            "name" : "Name 5",
-            "price" : 5.99,
-            "positions" : 2
-        },
-        {
-            "id" : 6,
-            "img" : "https://source.unsplash.com/random/200x200?sig=6",
-            "name" : "Name 6",
-            "price" : 6.99,
-            "positions" : 3
-        },
-        {
-            "id" : 7,
-            "img" : "https://source.unsplash.com/random/200x200?sig=7",
-            "name" : "Name 7",
-            "price" : 7.99,
-            "positions" : 2
-        },
-        {
-            "id" : 8,
-            "img" : "https://source.unsplash.com/random/200x200?sig=8",
-            "name" : "Name 8",
-            "price" : 8.99,
-            "positions" : 5
-        }
-    ])
+    const [cartDetails, setCartDetails] = useState({})
+    const [orders, setOrders] = useState([])
 
     return (
         <div className="container">
