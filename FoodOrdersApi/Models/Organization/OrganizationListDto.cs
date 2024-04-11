5d33a959b74dad803dda2cf0bf42564a2ef295ba
grochochotowski <g.m.rochowski@gmail.com>
2024-04-11 09:37:49 +0200

    create dtos for listing org an rest

---
+++ FoodOrdersApi/Models/Organization/OrganizationListDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FoodOrdersApi.Models.Organization
+{
+    public class OrganizationListDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ FoodOrdersApi/Models/Restaurant/RestaurantListDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FoodOrdersApi.Models.Restaurant
+{
+    public class RestaurantListDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
