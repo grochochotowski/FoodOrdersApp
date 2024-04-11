96073af32d0408f09dde5ea4e37a994aac9f5117
grochochotowski <g.m.rochowski@gmail.com>
2024-04-11 13:37:00 +0200

    update cart entity and dto model

--- FoodOrdersApi/Models/Cart/CartListDto.cs
+++ FoodOrdersApi/Models/Cart/CartListDto.cs
@@ -3,10 +3,12 @@
     public class CartListDto
     {
         public int Id { get; set; }
-        public string Organization { get; set; }
-        public string Restaurant { get; set; }
         public double TotalCartPrice { get; set; }
         public double MinPrice { get; set; }
         public double DeliveryPrice { get; set; }
+
+
+        public string Organization { get; set; }
+        public string Restaurant { get; set; }
     }
 }
---
+++ FoodOrdersApi/Models/Cart/DetailsCartDto.cs
@@ -0,0 +1,22 @@
+﻿using FoodOrdersApi.Models.Address;
+using FoodOrdersApi.Models.Order;
+
+namespace FoodOrdersApi.Models.Cart
+{
+    public class DetailsCartDto
+    {
+        public int Id { get; set; }
+        public double MinPrice { get; set; }
+        public double TotalCartPrice { get; set; }
+        public double DeliveryPrice { get; set; }
+        public double FreeDeliveryMinPrice { get; set; }
+        public string PhoneNumber { get; set; }
+        public string BankAccountNumber { get; set; }
+        public string? Note { get; set; }
+
+
+        public string Organization { get; set; }
+        public string Restaurant { get; set; }
+        public int AddressId { get; set; }
+    }
+}
