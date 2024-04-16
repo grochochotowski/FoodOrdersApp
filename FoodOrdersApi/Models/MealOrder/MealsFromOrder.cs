0d7bf124feaaba8f448818ec5aa11b1f2e9b16af
grochochotowski <g.m.rochowski@gmail.com>
2024-04-16 13:49:37 +0200

    create model to display meals from order

---
+++ FoodOrdersApi/Models/MealOrder/MealsFromOrder.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodOrdersApi.Models.MealOrder
+{
+    public class MealsFromOrder
+    {
+        public string Id { get; set; }
+        public string Img { get; set; }
+        public string Meal { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
