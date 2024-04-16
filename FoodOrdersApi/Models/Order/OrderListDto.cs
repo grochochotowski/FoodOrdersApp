908980e954db05aaee9994a27070d532cb5a32f0
grochochotowski <g.m.rochowski@gmail.com>
2024-04-16 11:41:24 +0200

    create dto model for list of orders

---
+++ FoodOrdersApi/Models/Order/OrderListDto.cs
@@ -0,0 +1,13 @@
+﻿namespace FoodOrdersApi.Models.Order
+{
+    public class OrderListDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Organization { get; set; }
+        public string Restaurant { get; set; }
+
+        public double Positions { get; set; }
+        public double TotalPrice { get; set; }
+    }
+}
