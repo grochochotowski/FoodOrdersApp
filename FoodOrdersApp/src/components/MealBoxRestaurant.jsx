7527f30b08b51d8d16fa54258e4c9d6912e2d178
grochochotowski <g.m.rochowski@gmail.com>
2024-04-17 22:49:17 +0200

    update sending message twhen clicking on meal

--- FoodOrdersApp/src/components/MealBoxRestaurant.jsx
+++ FoodOrdersApp/src/components/MealBoxRestaurant.jsx
@@ -7,30 +7,39 @@ import "../styles/App.css"
 export default function MealBoxRestaurant({meal}) {
     
     const [quantity, setQuantity] = useState(0);
+    const mealBoxRef = React.createRef();
 
     function updateQuantity(event) {
         let value = event.target.value;
         setQuantity(value);
+        console.log(quantity)
     }
 
     useEffect(() => {
-        function handleClickOutside(event) {
-          if (event.target.closest(".meal-box") && !event.target.closest(".quantity-input")) addMeal(meal.id)
-        }
-    
-        document.addEventListener("click", handleClickOutside);
-    
-        return () => {
-          document.removeEventListener("click", handleClickOutside);
+        const handleClickOutside = (event) => {
+          if (event.target.closest(".meal-box") && !event.target.closest(".quantity-input")) {
+            addMeal(meal.id);
+          }
         };
-    }, []);
+      
+        const mealBox = mealBoxRef.current;
+        if (mealBox) {
+          mealBox.addEventListener("click", handleClickOutside);
+        }
+      
+        return () => {
+          if (mealBox) {
+            mealBox.removeEventListener("click", handleClickOutside);
+          }
+        };
+      }, []);
 
     function addMeal(mealId) {
-        console.log("adding meal" + mealId)
+        console.log(`adding meal ${mealId}x${quantity}`);
     }
 
     return (
-        <div className="meal-box">
+        <div className="meal-box" ref={mealBoxRef}>
             <img src={meal.img ? meal.img : "https://i.pinimg.com/564x/ef/e8/d3/efe8d36db6281666a126189f05bfeff1.jpg"} alt="food-img" />
             <div className="meal-info">
                 <h5>{meal.name}</h5>
