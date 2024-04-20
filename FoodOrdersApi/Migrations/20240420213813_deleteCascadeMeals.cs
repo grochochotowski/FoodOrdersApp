db4e7f0c16d1c7f50f94610e9027c41bb7aa624e
grochochotowski <g.m.rochowski@gmail.com>
2024-04-20 23:40:54 +0200

    migrate db

---
+++ FoodOrdersApi/Migrations/20240420213813_deleteCascadeMeals.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodOrdersApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class deleteCascadeMeals : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Meals_Restaurants_RestaurantId",
+                table: "Meals");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Img",
+                table: "Meals",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Meals_Restaurants_RestaurantId",
+                table: "Meals",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Meals_Restaurants_RestaurantId",
+                table: "Meals");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Img",
+                table: "Meals",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Meals_Restaurants_RestaurantId",
+                table: "Meals",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- FoodOrdersApi/obj/Debug/net8.0/FoodOrdersApi.AssemblyInfo.cs
+++ FoodOrdersApi/obj/Debug/net8.0/FoodOrdersApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodOrdersApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d29f2a8eb4cefcd642d4d1e233c103ee4983ac42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22a2c8a5dc32163a5c9308d0fd99108b2041afee")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodOrdersApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodOrdersApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
