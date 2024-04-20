59259924ac2bcc8ee63e883c86fc6c9b84ba0878
grochochotowski <g.m.rochowski@gmail.com>
2024-04-21 00:09:40 +0200

    create rotate animation for fallback

--- FoodOrdersApp/src/components/Fallback.jsx
+++ FoodOrdersApp/src/components/Fallback.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
-import "./styles/fallback.css"
+import "../styles/fallback.css"
 
 export default function Fallback() {
     return (
         <div className='fallback'>
+            <div className="text">
+                <h1>Loading page</h1>
+                <h1 id="dot-1">.</h1>
+                <h1 id="dot-2">.</h1>
+                <h1 id="dot-3">.</h1>
+            </div>
             <div className="bg"></div>
-            <h1>Loading page...</h1>
         </div>
     )
 }
