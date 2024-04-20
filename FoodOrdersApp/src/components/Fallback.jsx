48d260d72308e508e70598cca9f8d13f50da4ad3
grochochotowski <g.m.rochowski@gmail.com>
2024-04-21 00:22:36 +0200

    create dots animation for fallback

--- FoodOrdersApp/src/components/Fallback.jsx
+++ FoodOrdersApp/src/components/Fallback.jsx
@@ -6,10 +6,7 @@ export default function Fallback() {
     return (
         <div className='fallback'>
             <div className="text">
-                <h1>Loading page</h1>
-                <h1 id="dot-1">.</h1>
-                <h1 id="dot-2">.</h1>
-                <h1 id="dot-3">.</h1>
+                <h1 class="loader"></h1>
             </div>
             <div className="bg"></div>
         </div>
