3c789962826b7705f01bb6761e7705abda3cfebb
grochochotowski <g.m.rochowski@gmail.com>
2024-04-23 15:04:25 +0200

    handle login inputs

--- FoodOrdersApp/src/pages/LogIn.jsx
+++ FoodOrdersApp/src/pages/LogIn.jsx
@@ -7,9 +7,26 @@ import "../styles/App.css"
 export default function LogIn() {
 
     const [isPasswordShown, setIsPasswordShown] = useState(false)
+    const [inputs, setInputs] = useState({
+        "login" : "",
+        "password" : ""
+    });
 
     function changePasswordVisibility() {
         setIsPasswordShown(prev => !prev)
+    }
+
+    function handleInputChange(inputId) {
+        setInputs(prev => (
+            {
+                ...prev,
+                [inputId]: document.getElementById(inputId).value
+            }
+        ))
+    }
+
+    async function login() {
+        console.log(inputs.login, " + ",  inputs.password)
     }
 
     return (
@@ -18,17 +35,27 @@ export default function LogIn() {
                 <h2>Log in to your account</h2>
                 <div className="login-input">
                     <label htmlFor="login">Login:</label>
-                    <input type="text" id="login"/>
+                    <input
+                        type="text"
+                        id="login"
+                        onChange={() => handleInputChange("login")}
+                        value={inputs.login}
+                    />
                 </div>
                 <div className="password-input">
                     <label htmlFor="password">Password:</label>
-                    <input type={isPasswordShown ? "text" : "password"} id="password"/>
+                    <input
+                        type={isPasswordShown ? "text" : "password"}
+                        id="password"
+                        onChange={() => handleInputChange("password")}
+                        value={inputs.password}
+                    />
                     {isPasswordShown
                     ? <i className="fa-regular fa-eye" onClick={() => changePasswordVisibility()}></i>
                     : <i className="fa-regular fa-eye-slash" onClick={() => changePasswordVisibility()}></i>
                     }
                 </div>
-                <button>Log In</button>
+                <button onClick={login}>Log In</button>
             </div>
         </main>
     )
