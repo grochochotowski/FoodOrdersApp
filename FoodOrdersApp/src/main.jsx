0ef96523135e322057ea7e82f6debbf1a4f52fe0
grochochotowski <g.m.rochowski@gmail.com>
2024-04-24 23:17:30 +0200

    creae auth provider

--- FoodOrdersApp/src/main.jsx
+++ FoodOrdersApp/src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { AuthProvider } from './context/AuthProvider.jsx'
 
 import App from './App.jsx'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Router>
-            <App />
-        </Router>
+        <AuthProvider>
+            <Router>
+                <App />
+            </Router>
+        </AuthProvider>
     </React.StrictMode>,
 )
